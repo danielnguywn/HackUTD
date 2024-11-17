f31a3ffde99dd565a4a48242f27799ed4bf49fdb
Sunay Shehaan <sunayshehaan@Sunays-Laptop.local>
2024-11-17 02:11:01 -0600

    chatbot logic

--- backend/router.js
+++ backend/router.js
@@ -3,6 +3,8 @@ const express = require("express");
 const router = express.Router();
 require ('dotenv').config();
 const mongoose = require('mongoose');
+const OpenAI = require('openai')
+const axios = require('axios')
 
 
 // create user
@@ -35,7 +37,54 @@ router.get('/:email', async (req,res) => {
   })
 
 
-router.patch()
+  //chatbot history
+router.post('/chatbot',async (req,res)=>{
+  const {email, userInput} =  req.body
+
+  const Userdata = await UserProfile.findOne({"User.PersonalInfo.email":email})
+
+  const chatHistory = Userdata.AccountInfo.History
+
+  const headers = {
+    'Authorization': `Bearer ${process.env.SAMBANOVA_APIKEY}`,
+    'Content-Type': 'application/json'
+  };
+    try {
+      const requestBody = {
+        model: "Meta-Llama-3.1-8B-Instruct",
+        messages: [
+          { role: "system", content: "You are a helpful assistant." },
+          ...chatHistory,
+          { role: "user", content: userInput }
+        ],
+        temperature: 0.7, 
+        max_tokens: 100
+      };
+  
+      
+      const response = await axios.post("https://api.sambanova.ai/v1/chat/completions", requestBody, { headers });
+  
+      const completionText = response.data.choices?.[0]?.message?.content || 'No response';
+
+      Userdata.AccountInfo.History.push({role:"user",content:userInput})
+      Userdata.AccountInfo.History.push({role:"assistant",content:completionText})
+
+      await Userdata.save()
+
+
+      console.log('SambaNova Completion:', completionText);
+
+
+      res.json({"botresponse":completionText})
+
+  } catch (error) {
+    console.error('Error with SambaNova API request:', error.response?.data || error.message);
+  }
+
+
+
+})
+
 
 
 module.exports = router;
