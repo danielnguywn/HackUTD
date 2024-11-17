6915004817727eff2ea4f7024425ae825894a743
Sunay Shehaan <sunayshehaan@Sunays-Laptop.local>
2024-11-16 19:15:28 -0600

    added server to main

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log("Hello")
+const express=require('express')
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const app=express()
+
+app.use(express.json())
+
+const server = app.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});
+
+const connectToMongoose = async () => {
+  try {
+      await mongoose.connect(process.env.MONGO_URI);
+      console.log('Connected to MongoDB');
+  } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+connectToMongoose()
