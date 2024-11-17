66dc162ac95537c4a2e6e83aef8b516774bf9f51
danielnguywn <ddanielnnguyen17@gmail.com>
2024-11-16 21:17:41 -0600

    schema and router

--- backend/UserSchema.js
+++ backend/UserSchema.js
@@ -2,22 +2,73 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const profileSchema = new Schema({
+const UserSchema = new Schema({
     PersonalInfo: {
-        FirstName: {type: String, required: true},
-        LastName: {type: String, required: true},
-        Email: {type: String, required: true},
+        FirstName: {
+            type: String, 
+            required: true
+        },
+        LastName: {
+            type: String, 
+            required: true
+        },
+        Email: {
+            type: String, 
+            required: true
+        },
+        Password: {
+            type: String, 
+            required: true
+        },
     },
     AccountInfo: {
-        Occupation: {type: String, required: true},
-        Frequency: {type: String, required: true},
-        Services: {type: String, required: true},
-        IncomeMethod: {type: String, required: true},
-        Savings: {type: String, required: true},
-        Goal: {type: String, required: true},
-        Budget: {type: String, required: true},
-        Deposit: {type: String, required: true},
-    }
+        Occupation: {
+            type: String, 
+            required: true
+        },
+        Frequency: {
+            type: String, 
+            required: true
+        },
+        Services: {
+            type: String, 
+            required: true
+        },
+        IncomeMethod: {
+            type: String, 
+            required: true
+        },
+        Savings: {
+            type: String, 
+            required: true
+        },
+        Goal: {
+            type: String, 
+            required: true
+        },
+        Budget: {
+            type: String, 
+            required: true
+        },
+        Deposit: {
+            type: String, 
+            required: true
+        },
+    },
+    BankInfo: {
+        BankName: {
+            type: String, 
+            required: true
+        },
+        AccountNumber: {
+            type: String, 
+            required: true
+        },
+        RoutingNumber: {
+            type: String, 
+            required: true
+        },
+    },
 }, {timestamps: true});
 
-module.exports = mongoose.model("UserProfile", profileSchema);
+module.exports = mongoose.model("UserProfile", UserSchema);
